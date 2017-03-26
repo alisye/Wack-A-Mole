393b4fadb351b1a2c2b59f04e1c95a4a0f7b6dd8
Ian Berlot-Attwell <ian.berlot.attwell@mail.utoronto.ca>
2017-03-25 20:03:51 -0400

    Made some fixes to total score

--- MoleRL/TotalScore.v
+++ MoleRL/TotalScore.v
@@ -1,15 +1,17 @@
-module CompleteCount(reset, clock, clock_wait, clockrL, control, mole_hit, Mheight, rise, totalScore, totalRise);
+module CompleteCount(reset, clock, clock_wait, clockrL, control, mole_hit, Mheight, totalScore, totalRise);
 	input reset;
 	input clock;
+	input clock_wait;
+	input clockrl;
 	input [7:0] control;
 	input [7:0] mole_hit;
-	input [7:0] rise;
 
-	output [4:0] Mheight;
+	output [39:0] Mheight;
 	output [15:0] totalScore;
 	output [15:0] totalRise;
 
 	wire [7:0] hiding;
+	
 	wire [4:0] Mheight0;
 	wire [4:0] Mheight1;
 	wire [4:0] Mheight2;
@@ -37,7 +39,6 @@ module CompleteCount(reset, clock, clock_wait, clockrL, control, mole_hit, Mheig
 	wire [15:0] riseCountToAdder6;
 	wire [15:0] riseCountToAdder7;
 
-
 	wire [15:0] AdderConnecter0;
 	wire [15:0] AdderConnecter1;
 	wire [15:0] AdderConnecter2;
@@ -52,132 +53,70 @@ module CompleteCount(reset, clock, clock_wait, clockrL, control, mole_hit, Mheig
 	wire [15:0] AdderConnecter11;
 
 	rl mole0(.reset(reset), .CLOCK_50(clock), .CLOCK_RL(clockrL), .CLOCK_WAIT(clock_wait), .go(control[0]), .hiding(hiding[0]), .Mheight(Mheight0));
-
 	rl mole1(.reset(reset), .CLOCK_50(clock), .CLOCK_RL(clockrL), .CLOCK_WAIT(clock_wait), .go(control[1]), .hiding(hiding[1]), .Mheight(Mheight1));
-
 	rl mole2(.reset(reset), .CLOCK_50(clock), .CLOCK_RL(clockrL), .CLOCK_WAIT(clock_wait), .go(control[2]), .hiding(hiding[2]), .Mheight(Mheight2));
-
 	rl mole3(.reset(reset), .CLOCK_50(clock), .CLOCK_RL(clockrL), .CLOCK_WAIT(clock_wait), .go(control[3]), .hiding(hiding[3]), .Mheight(Mheight3));
-
 	rl mole4(.reset(reset), .CLOCK_50(clock), .CLOCK_RL(clockrL), .CLOCK_WAIT(clock_wait), .go(control[4]), .hiding(hiding[4]), .Mheight(Mheight4));
-
 	rl mole5(.reset(reset), .CLOCK_50(clock), .CLOCK_RL(clockrL), .CLOCK_WAIT(clock_wait), .go(control[5]), .hiding(hiding[5]), .Mheight(Mheight5));
-
 	rl mole6(.reset(reset), .CLOCK_50(clock), .CLOCK_RL(clockrL), .CLOCK_WAIT(clock_wait), .go(control[6]), .hiding(hiding[6]), .Mheight(Mheight6));
-
 	rl mole7(.reset(reset), .CLOCK_50(clock), .CLOCK_RL(clockrL), .CLOCK_WAIT(clock_wait), .go(control[7]), .hiding(hiding[7]), .Mheight(Mheight7));
 	
 	assign Mheight = {Mheight0, Mheight1, Mheight2, Mheight3, Mheight4, Mheight5, Mheight6, Mheight7};
 
-	scoreCount scoreMole0(.count(scoreCountToAdder0), .control(control[0]), .hiding(hiding[0]), .reset(reset));
-	
-	scoreCount scoreMole1(.count(scoreCountToAdder1), .control(control[1]), .hiding(hiding[1]), .reset(reset));
-	
-	scoreCount scoreMole2(.count(scoreCountToAdder2), .control(control[2]), .hiding(hiding[2]), .reset(reset));
-	
-	scoreCount scoreMole3(.count(scoreCountToAdder3), .control(control[3]), .hiding(hiding[3]), .reset(reset));
-	
-	scoreCount scoreMole4(.count(scoreCountToAdder4), .control(control[4]), .hiding(hiding[4]), .reset(reset));
-	
-	scoreCount scoreMole5(.count(scoreCountToAdder5), .control(control[5]), .hiding(hiding[5]), .reset(reset));
-	
-	scoreCount scoreMole6(.count(scoreCountToAdder6), .control(control[6]), .hiding(hiding[6]), .reset(reset));
-	
-	scoreCount scoreMole7(.count(scoreCountToAdder7), .control(control[7]), .hiding(hiding[7]), .reset(reset));
+	scoreCount scoreMole0(.count(scoreCountToAdder0), .mole_hit(mole_hit[0]), .hiding(hiding[0]), .reset(reset));
+	scoreCount scoreMole1(.count(scoreCountToAdder1), .mole_hit(mole_hit[1]), .hiding(hiding[1]), .reset(reset));
+	scoreCount scoreMole2(.count(scoreCountToAdder2), .mole_hit(mole_hit[2]), .hiding(hiding[2]), .reset(reset));
+	scoreCount scoreMole3(.count(scoreCountToAdder3), .mole_hit(mole_hit[3]), .hiding(hiding[3]), .reset(reset));
+	scoreCount scoreMole4(.count(scoreCountToAdder4), .mole_hit(mole_hit[4]), .hiding(hiding[4]), .reset(reset));
+	scoreCount scoreMole5(.count(scoreCountToAdder5), .mole_hit(mole_hit[5]), .hiding(hiding[5]), .reset(reset));
+	scoreCount scoreMole6(.count(scoreCountToAdder6), .mole_hit(mole_hit[6]), .hiding(hiding[6]), .reset(reset));
+	scoreCount scoreMole7(.count(scoreCountToAdder7), .mole_hit(mole_hit[7]), .hiding(hiding[7]), .reset(reset));
 	
 	DecimalAdder4Dig scoreAdd0(.A(scoreCountToAdder0), .B(scoreCountToAdder1), .out(AdderConnecter0));
-	
 	DecimalAdder4Dig scoreAdd1(.A(AdderConnecter0), .B(scoreCountToAdder2), .out(AdderConnecter1));
-
 	DecimalAdder4Dig scoreAdd2(.A(AdderConnecter1), .B(scoreCountToAdder3), .out(AdderConnecter2));
-
 	DecimalAdder4Dig scoreAdd3(.A(AdderConnecter2), .B(scoreCountToAdder4), .out(AdderConnecter3));
-
 	DecimalAdder4Dig scoreAdd4(.A(AdderConnecter3), .B(scoreCountToAdder5), .out(AdderConnecter4));
-
 	DecimalAdder4Dig scoreAdd5(.A(AdderConnecter4), .B(scoreCountToAdder6), .out(AdderConnecter5));
-
 	DecimalAdder4Dig scoreAdd6(.A(AdderConnecter5), .B(scoreCountToAdder7), .out(totalScore));
 
-	riseCount riseMole0(.count(riseCountToAdder0), .control(control[0]), .rise(rise[0]), .reset(reset));
-	riseCount riseMole1(.count(riseCountToAdder1), .control(control[1]), .rise(rise[1]), .reset(reset));
-	riseCount riseMole2(.count(riseCountToAdder2), .control(control[2]), .rise(rise[2]), .reset(reset));
-	riseCount riseMole3(.count(riseCountToAdder3), .control(control[3]), .rise(rise[3]), .reset(reset));
-	riseCount riseMole4(.count(riseCountToAdder4), .control(control[4]), .rise(rise[4]), .reset(reset));
-	riseCount riseMole5(.count(riseCountToAdder5, .control(control[5]), .rise(rise[5]), .reset(reset));
-	riseCount riseMole6(.count(riseCountToAdder6), .control(contorl[6]), .rise(rise[6]), .reset(reset));
-	riseCount riseMole7(.count(riseCountToAdder7), .control(control[7]), .rise(rise[7]), .reset(reset));
+	riseCount riseMole0(.count(riseCountToAdder0), .control(control[0]), .hiding(hiding[0]), .reset(reset));
+	riseCount riseMole1(.count(riseCountToAdder1), .control(control[1]), .hiding(hiding[1]), .reset(reset));
+	riseCount riseMole2(.count(riseCountToAdder2), .control(control[2]), .hiding(hiding[2]), .reset(reset));
+	riseCount riseMole3(.count(riseCountToAdder3), .control(control[3]), .hiding(hiding[3]), .reset(reset));
+	riseCount riseMole4(.count(riseCountToAdder4), .control(control[4]), .hiding(hiding[4]), .reset(reset));
+	riseCount riseMole5(.count(riseCountToAdder5), .control(control[5]), .hiding(hiding[5]), .reset(reset));
+	riseCount riseMole6(.count(riseCountToAdder6), .control(contorl[6]), .hiding(hiding[6]), .reset(reset));
+	riseCount riseMole7(.count(riseCountToAdder7), .control(control[7]), .hiding(hiding[7]), .reset(reset));
 
 	DecimalAdder4Dig riseAdd0(.A(riseCountToAdder0), .B(riseCountToAdder1), .out(AdderConnecter6));
-
 	DecimalAdder4Dig riseAdd1(.A(AdderConnecter6), .B(riseCountToAdder2), .out(AdderConnecter7));
-
 	DecimalAdder4Dig riseAdd2(.A(AdderConnecter7), .B(riseCountToAdder3), .out(AdderConnecter8));
-
 	DecimalAdder4Dig riseAdd3(.A(AdderConnecter8), .B(riseCountToAdder4), .out(AdderConnecter9));
-
 	DecimalAdder4Dig riseAdd4(.A(AdderConnecter9), .B(riseCountToAdder5), .out(AdderConnecter10));
-
 	DecimalAdder4Dig riseAdd5(.A(AdderConnecter10), .B(riseCountToAdder6), .out(AdderConnecter11));
-
 	DecimalAdder4Dig riseAdd6(.A(AdderConnecter11), .B(riseCountToAdder7), .out(totalRise));
 
 endmodule
 	
 
 //change clocks of both counters to the and of hiding and control
-module riseCount(count, control, rise, reset);
+module riseCount(count, control, hiding, reset);
 	output [15:0] count;
-	input reset;
+	input reset, hiding, countrol;
 	
-	reg [3:0] ones;
-	reg [3:0] hundreds;
-	reg [3:0] tens;
-	reg [3:0] thousands;
-	
-	wire clock = (control && rise);
-	assign count = {ones, tens, hundreds, thousands};
-	
-	always @(clock) begin
-		if(reset == 1'b1)
-			ones <= 4'b0;
-		else if(ones == 4'd9)
-			ones <= 4'b0;
-		else
-			ones <= ones + 1'b1;
-	end
-	
-	always @(clock) begin
-		if(reset == 1'b1)
-			tens <= 4'b0;
-		else if(ones == 4'd9 && tens == 4'd9)
-			tens <= 4'b0;
-		else if(ones == 4'd9)
-			tens <= tens + 1'b1;
-	end
-	
-	always @(clock) begin
-		if(reset == 1'b1 )
-			hundreds <= 4'b0;
-		else if({hundreds, tens, ones} == 12'b1001_1001_1001)
-			hundreds <= 4'b0;
-		else if(tens == 4'd9 && ones == 4'd9)
-			hundreds <= hundreds + 1'b1;
-	end
-	
-	always @(clock) begin
-		if(reset == 1'b1)
-			thousands <= 4'b0;
-		if({thousands, hundreds, tens, ones} == 16'b1001_1001_1001_1001)
-			thousands <= 4'b0;
-		else if(hundreds == 4'd9 && tens == 4'd9 && ones == 4'd9)
-			thousands <= thousands + 1'b1;
-	end
-
+	DecimalCounter4Dig dc4g (.count(count), .clock(control && hiding), .reset(reset));
 endmodule
 
 
-module scoreCount(count, control, hiding, reset);
+module scoreCount(count, mole_hit, hiding, reset);
+	output [15:0] count;
+	input reset, hiding, mole_hit;
+	
+	DecimalCounter4Dig dc4g (.count(count), .clock(mole_hit && ~hiding), .reset(reset));
+endmodule
+
+module DecimalCounter4Dig(count, clock, reset);
 	output [15:0] count;
 	input clock;
 	input reset;
@@ -187,10 +126,9 @@ module scoreCount(count, control, hiding, reset);
 	reg [3:0] tens;
 	reg [3:0] thousands;
 	
-	wire clock = (control && hiding);
 	assign count = {ones, tens, hundreds, thousands};
 	
-	always @(clock) begin
+	always @(posedge clock) begin
 		if(reset == 1'b1)
 			ones <= 4'b0;
 		else if(ones == 4'd9)
@@ -199,7 +137,7 @@ module scoreCount(count, control, hiding, reset);
 			ones <= ones + 1'b1;
 	end
 	
-	always @(clock) begin
+	always @(posedge clock) begin
 		if(reset == 1'b1)
 			tens <= 4'b0;
 		else if(ones == 4'd9 && tens == 4'd9)
@@ -208,7 +146,7 @@ module scoreCount(count, control, hiding, reset);
 			tens <= tens + 1'b1;
 	end
 	
-	always @(clock) begin
+	always @(posedge clock) begin
 		if(reset == 1'b1 )
 			hundreds <= 4'b0;
 		else if({hundreds, tens, ones} == 12'b1001_1001_1001)
@@ -217,7 +155,7 @@ module scoreCount(count, control, hiding, reset);
 			hundreds <= hundreds + 1'b1;
 	end
 	
-	always @(clock) begin
+	always @(posedge clock) begin
 		if(reset == 1'b1)
 			thousands <= 4'b0;
 		if({thousands, hundreds, tens, ones} == 16'b1001_1001_1001_1001)
